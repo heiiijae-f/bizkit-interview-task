55228c824415dccc65339a9635fd52de7020fff1
AJ Flora <139091475+heiiijae-f@users.noreply.github.com>
2023-07-12 15:55:52 +0800

    Updated match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,8 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    set_1 = set(fave_numbers_1)
+    set_2 = set(fave_numbers_2)
+    return set_2.issubset(set_1)
 
     return True
